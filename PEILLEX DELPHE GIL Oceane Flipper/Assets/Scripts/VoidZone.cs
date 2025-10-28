bda9399c900db57b5e25b0aa295f837537399d47
OceanePe <o.peillexdelphegil@gmail.com>
2025-10-28 23:01:31 +0100

    VoidZone Space to escape

--- PEILLEX DELPHE GIL Oceane Flipper/Assets/Scripts/VoidZone.cs
+++ PEILLEX DELPHE GIL Oceane Flipper/Assets/Scripts/VoidZone.cs
@@ -6,7 +6,7 @@ public class VoidZone : MonoBehaviour
 	public List<Transform> ballsTransforms;
     public float           strength      = 1;
     public int             scoreToAdd    = 10;
-    public float             strenghtBalls = 2;
+    public float             strengthBalls = 2;
     void OnTriggerEnter(Collider other)
     {
         ballsTransforms.Add(other.transform);
@@ -24,16 +24,21 @@ public class VoidZone : MonoBehaviour
         for (int i = 0; i < ballsTransforms.Count; i++)
         {
             Vector3 force = transform.position - ballsTransforms[i].position;
-            ballsTransforms[i].GetComponent<Rigidbody>().AddForce(force*strength, ForceMode.Impulse);
+            ballsTransforms[i].GetComponent<Rigidbody>().AddForce(force*strength, ForceMode.Force);
+        }
+        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            for (int i = 0; i < ballsTransforms.Count; i++)
+            {
+                Vector3 pushBall = ballsTransforms[i].position - transform.position;
+                ballsTransforms[i].GetComponent<Rigidbody>().AddForce(pushBall*strengthBalls, ForceMode.Impulse);
+            }
         }
         
     }
-
-    void Update(float strengthBalls)
-    {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            Vector3 force = transform.up * strengthBalls;
-        }
-    }
+    
+    
+       
+    
 }
