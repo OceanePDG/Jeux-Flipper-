a4b9a72c498923cfece14afa67a95ccc196f37f1
OceanePDG <seila@laposte.net>
2025-10-29 21:22:39 +0100

    Avancer debut shader

---
+++ PEILLEX DELPHE GIL Oceane Flipper/Assets/Scripts/MenuController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuController : MonoBehaviour
+{
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
