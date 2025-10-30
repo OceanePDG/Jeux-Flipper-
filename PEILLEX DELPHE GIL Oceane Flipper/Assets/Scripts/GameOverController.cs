04c17ae0934bf5d08a9d187ca77c97c3d9693da3
OceanePDG <seila@laposte.net>
2025-10-30 17:38:20 +0100

    test

---
+++ PEILLEX DELPHE GIL Oceane Flipper/Assets/Scripts/GameOverController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+        Time.timeScale = 1;
+        
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
